26a8f57bbbbbba768cb469e4da94397f1b00877f
ThaysMrtns <thays.martins@nave.org.br>
2021-06-25 17:29:51 -0300

    section2

--- pages/lp_indefesos/components/section_2.js
+++ pages/lp_indefesos/components/section_2.js
@@ -24,14 +24,3 @@ export default function Section2(){
         </div>
     );
 }
-/*
-A casa é um lugar muito importante. Ela nos abriga da chuva, 
-faz sombra em dia de sol e permite que a gente durma de forma confortável.
- Assim como a gente, os animais também merecem um lar, um lugarzinho aconchegante para viver.
-  É por isso que ONG Os Indefesos, que cuida dos animais resgatados 
-  das ruas ou em situação precária, criou o projeto “Meu Tijolinho” 
-  que tem o intuito de construir um abrigo mais confortável, 
-  tranquilo e acolhedor para todos os animais. Com uma pequena ajuda,
-   podemos mudar a vida de milhares de peludinhos.
- Assim, eles poderão ter o canil que sempre sonharam e merecem!
-*/ 
