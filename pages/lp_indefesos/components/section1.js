111842cb82739870453852b6a0aa98a1ce133e35
ThaysMrtns <thays.martins@nave.org.br>
2021-06-25 15:28:23 -0300

    section1

--- pages/lp_indefesos/components/section1.js
+++ pages/lp_indefesos/components/section1.js
@@ -1,14 +1,19 @@
 import style from "../../../styles/lp_indefesos/indefesos.module.sass";
 import background from "../../../public/cachorros-juntos.png";
-import Image from "next/image";
 
 export default function Section1(){
   return (
     <div className={style.section1}>
       <div
         className={style.background}
-        src={background}
-      />
+        src={background}>
+          
+        <div className={style.description}>
+          <h1 className={style.title}>Livima + Os Indefesos</h1>
+          <p className={style.text}>Para ajudar os animaizinhos indefesos, contamos com sua ajuda. A cada anúncio ativado na Livima, a gente doa 1 real para viabilizar a construção do projeto Meu Tijolinho.</p>
+        </div>
+        
+      </div>
     </div>
   );
 }
