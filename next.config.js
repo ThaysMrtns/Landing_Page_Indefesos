b7a0ec6d7919a5b0e719701fa0dc28586c67e277
ThaysMrtns <thays.martins@nave.org.br>
2021-07-01 11:47:51 -0300

    google fonts

--- next.config.js
+++ next.config.js
@@ -2,3 +2,14 @@ const withImages = require('next-images')
 module.exports = {
   reactStrictMode: true,
 }
+
+// Goggle fonts
+const { withGoogleFonts } = require("nextjs-google-fonts");
+
+module.exports = withGoogleFonts({
+  googleFonts: {
+    fonts: [
+      "https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,900;1,400;1,700&display=swap",
+    ],
+  },
+});
